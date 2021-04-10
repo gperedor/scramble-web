8559613788bc2f3ab48e71ddba11abf833ec6c56
Gabriel Peredo <gabrielperedo@Gabriels-MacBook-Pro.local>
2021-04-10 08:07:15 -0400

    Build system setup
    For support of the backend application

--- project.clj
+++ project.clj
@@ -6,28 +6,37 @@
 
   :min-lein-version "2.7.1"
 
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [;; Backend
+                 [org.clojure/clojure "1.10.0"]
+
+                 [ring "1.8.1"]
+                 [metosin/reitit "0.5.12"]
+                 [metosin/jsonista "0.3.1"]
+                 [clj-scramble "0.1.0-SNAPSHOT"]
+                 [ring/ring-mock "0.4.0"]
+                 [ring/ring-jetty-adapter "1.8.1"]
+
+                 ;; Frontend
                  [org.clojure/clojurescript "1.10.773"]
 
-                 ;;; Frontend
                  [reagent "1.0.0"]
                  [hiccup "1.0.5"]
                  [cljs-http "0.1.46"]
 
                  ;; misc
                  [fipp "0.6.23"]]
-
+  
   :source-paths ["src"]
 
-  :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
+  :aliases {"check-run" ["do" "clean," "test," "fig:test," "run"]
+            "fig"       ["trampoline" "run" "-m" "figwheel.main"]
             "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
             "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
             "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" "frontend.test-runner"]}
 
   :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.12"]
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]]
-                   
+                   :main ^:skip-aot backend.server
                    :resource-paths ["target"]
                    ;; need to add the compiled assets to the :clean-targets
                    :clean-targets ^{:protect false} ["target"]}})
-
