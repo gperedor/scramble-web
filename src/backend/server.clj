c7b598991a23341230effe934621687f35ba4967
Gabriel Peredo <gabrielperedo@Gabriels-MacBook-Pro.local>
2021-04-10 08:05:57 -0400

    Fixing of routes, por parameter
    Swagger routes were broken

--- src/backend/server.clj
+++ src/backend/server.clj
@@ -9,6 +9,7 @@
             [reitit.ring.middleware.exception :as exception]
             [reitit.ring.middleware.multipart :as multipart]
             [reitit.ring.middleware.parameters :as parameters]
+            [hiccup.page :refer [html5 include-js include-css]]
             [ring.adapter.jetty :as jetty]
             [muuntaja.core :as m]
             [clj-scramble.core :as s])
@@ -41,25 +42,31 @@
      ::exception/wrap (fn [handler e request]
                         (println "ERROR" (pr-str (:uri request)))
                         (handler e request))})))
-
 (def app
   (ring/ring-handler
-    (ring/router
-      [["/swagger.json"
+   (ring/router
+
+     [["/swagger.json"
         {:get {:no-doc true
                :swagger {:info {:title "scramble-api"
                                 :description "The Flexiana coding test"}}
                :handler (swagger/create-swagger-handler)}}]
 
-       ["/scramble"
-        {:get {:summary "Check if the first string can be scrambled to contain the second"
-               :parameters {:query {:str1 string?, :str2 string?}}
-               :responses {200 {:body {:scrambled boolean?}}}
-               :handler (fn [{{{:keys [str1 str2]} :query} :parameters}]
-                          {:status 200
-                           :body {:scrambled (s/scramble? str1 str2)}})}}]]
-      {:exception pretty/exception
-       :data {:coercion reitit.coercion.spec/coercion
+      ["/api-docs/*"
+       {:get {:no-doc true
+              :handler (swagger-ui/create-swagger-ui-handler)}}]
+
+      ["/scramble"
+       {:get {:summary "Check if the first string can be scrambled to contain the second"
+              :parameters {:query {:str1 string?, :str2 string?}}
+              :responses {200 {:body {:scrambled boolean?}}}
+              :handler (fn [{{{:keys [str1 str2]} :query} :parameters}]
+                         {:status 200
+                          :body {:scrambled (s/scramble? str1 str2)}})}}]]
+
+     {:exception pretty/exception
+      :conflicts (constantly nil)
+      :data {:coercion reitit.coercion.spec/coercion
               :muuntaja m/instance
               :middleware [;; swagger feature
                            swagger/swagger-feature
@@ -79,16 +86,14 @@
                            coercion/coerce-request-middleware
                            ;; multipart
                            multipart/multipart-middleware]}})
-    (ring/routes
-      (swagger-ui/create-swagger-ui-handler
-        {:path "/"
-         :config {:validatorUrl nil
-                  :operationsSorter "alpha"}})
-      (ring/create-default-handler))))
+   (ring/routes
+    (ring/create-resource-handler {:path "/"})
+    (ring/create-default-handler))))
 
 (defn start []
-  (jetty/run-jetty #'app {:port 3000, :join? false})
-  (println "server running in port 3000"))
+  (let [port (or (System/getenv "SERVER_PORT") 3000)]
+    (jetty/run-jetty #'app {:port port, :join? false})
+    (println (str "server running in port " port))))
 
 (defn -main []
   (start))
